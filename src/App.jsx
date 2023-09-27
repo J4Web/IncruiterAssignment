8bc21ad9292f0e7097c407427cc3d706375da3a5
j4web <prakharsrivastava841@gmail.com>
2023-09-27 22:01:37 +0530

    init proj

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+
+export default function App() {
+  return (<>
+  <h1 className="w-screen h-screen bg-red-200">
+      Hello world!
+    </h1>
+    <h2>
+      ertfghjkl;
+    </h2>
+  </>
+    
+  )
+}
